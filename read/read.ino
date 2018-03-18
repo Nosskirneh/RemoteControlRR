4e73013792ee3a49fb5a18aa126bccfeea9243c8
Andreas Henriksson <andreas.karl.henriksson@gmail.com>
2018-03-18 11:52:38 +0100

    Configured simple messages

--- read/read.ino
+++ read/read.ino
@@ -4,20 +4,37 @@
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
+typedef enum MessageType {
+    ManualMode = 0b00000000,
+    RemoteMode = 0b00100000,
+    SteerRight = 0b10000000,
+    SteerLeft  = 0b10100000,
+    Accelerate = 0b11000000,
+} MessageType;
+
+
 void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+    Serial.begin(9600);
+    radio.begin();
+    radio.openReadingPipe(0, address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.startListening();
 }
 
 void loop() {
-  delay(1000);
-  Serial.println("trying to read...");
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+    delay(1000);
+    Serial.println("trying to read...");
+    if (radio.available()) {
+        byte text;
+        radio.read(&text, sizeof(text));
+        Serial.println(text);
+
+        if (text == SteerRight) {
+            Serial.println("Read right message!");
+        } else if (text == SteerLeft) {
+            Serial.println("Read left message!");
+        } else if (text == Accelerate) {
+            Serial.println("Read accelerate message!");
+        }
+    }
 }
--- send/send.ino
+++ send/send.ino
@@ -4,17 +4,47 @@
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
+typedef enum MessageType {
+    ManualMode = 0b00000000,
+    RemoteMode = 0b00100000,
+    SteerRight = 0b10000000,
+    SteerLeft  = 0b10100000,
+    Accelerate = 0b11000000,
+} MessageType;
+
+
 void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+}
+
+// Replace this with joystick
+void readSerial() {
+    if (Serial.available()) {
+        char c = toupper(Serial.read());
+        byte message = 0;
+        if (c == 'R') {
+            Serial.println("Sending right message!");
+            message = SteerRight;
+        } else if (c == 'L') {
+            Serial.println("Sending left message!");
+            message = SteerLeft;
+        } else if (c == 'A') {
+            Serial.println("Sending accelerate message!");
+            message = Accelerate;
+        }
+
+        Serial.println(message);
+        radio.write(&message, sizeof(message));
+    }
 }
 
 void loop() {
-  delay(1000);
-  Serial.println("triyng to send...");
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+    delay(1000);
+    Serial.println("Waiting for input...");
+
+    readSerial();
 }
