b545f4624fed8ecb5c87a707f1e73a5869d25c8c
Andreas Henriksson <andreas.karl.henriksson@gmail.com>
2018-04-12 17:33:46 +0200

    Added code for digital potentiometer

--- read/read.ino
+++ read/read.ino
@@ -8,9 +8,18 @@
 
 #include <SPI.h>
 #include <RF24.h>
+#include <MCP41_Simple.h>
+
+#define RADIO_CE  7
+#define RADIO_CSN 8
+
+#define POTENTIOMETER_CS 53
+
 
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
+
+MCP41_Simple potentiometer;
 
 // To be sent over radio
 typedef enum MessageType {
@@ -24,19 +33,35 @@ int mode = ManualMode;
 
 void setup() {
     Serial.begin(9600);
+    // Radio
     radio.begin();
     radio.openReadingPipe(0, address);
     radio.setPALevel(RF24_PA_MIN);
     radio.startListening();
+
+    // Potentiometer
+    potentiometer.begin(POTENTIOMETER_CS);
+    potentiometer.setWiper(0);
 }
 
 int nextRadioMessage() {
     int message;
     radio.read(&message, sizeof(message));
+    return message;
+}
+
+void updateAccelerationValue(int acc) {
+    if (acc >= 0 && acc <= 100) {
+        double wiper = percentageToStep(acc);
+        potentiometer.setWiper(wiper);
+    }
+}
+
+double percentageToStep(double percentage) {
+    return percentage * 0.88 + 20;
 }
 
 void readRadio() {
-    Serial.println("Will read!");
     static unsigned long lastMessageReceivedTime = 0;
     if (radio.available()) {
         lastMessageReceivedTime = millis();
@@ -51,22 +76,24 @@ void readRadio() {
             Serial.println(mode);
         } else if (header == Steer) {
             mode = RemoteMode;
-            Serial.println("Read steering message!");
+            Serial.print("Read steering message: ");
             int angle = message & 0xFF;
             Serial.println(angle, DEC);
         } else if (header == Accelerate) {
             mode = RemoteMode;
-            Serial.println("Read accelerate message!");
+            Serial.print("Read accelerate message: ");
             int acc = message & 0xFF;
+            updateAccelerationValue(acc);
             Serial.println(acc);
         }
     } else if (mode != ManualMode && millis() - lastMessageReceivedTime > 5000) {
         Serial.println("No radio messages received for 5 seconds, falling back to manual mode!");
         mode = ManualMode;
+        updateAccelerationValue(0);
     }
 }
 
 void loop() {
-    delay(1000);
+    delay(100);
     readRadio();
 }
